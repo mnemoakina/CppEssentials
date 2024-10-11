4775ee57c9cb830160b729f61ca06a40be5999b1
M.Dipaulo <dipaulodev@gmail.com>
2024-10-11 14:09:16 -0300

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define PI 3.14159
+
+const char* _CLAVE = "GRONE";
+const char CAR = 'A';
+
+int main()
+{    
+    printf("%.5f-%s-%c",PI,_CLAVE,CAR);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    printf("\n");
+    return 0;
+}
